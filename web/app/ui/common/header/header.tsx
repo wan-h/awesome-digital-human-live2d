6adf43cb2abb747e10333b52be12413c71d4246a
aliyan404 <1434451678@qq.com>
2024-09-15 23:19:36 +0800

    feat: add menu for mobile

--- web/app/ui/common/header/header.tsx
+++ web/app/ui/common/header/header.tsx
@@ -1,60 +1,109 @@
 'use client'
 
-import { useEffect } from "react";
-import { useHeartbeatStore } from "@/app/lib/store";
-import { HeadAlert } from "@/app/ui/common/alert";
-import { PROJ_NAME, HEART_BEAT_ALERT, HEART_BEAT_CHECK_1S } from "@/app/lib/constants";
-import { WindowMenu } from "./menu";
-import Github from "./github";
-import Link from "next/link";
-import useWebSocket, { ReadyState } from 'react-use-websocket';
-import { get_heatbeat_wss } from '@/app/lib/api';
-
-
+import { useState, useEffect } from 'react'
+import { useHeartbeatStore } from '@/app/lib/store'
+import { HeadAlert } from '@/app/ui/common/alert'
+import {
+  PROJ_NAME,
+  HEART_BEAT_ALERT,
+  HEART_BEAT_CHECK_1S,
+} from '@/app/lib/constants'
+import { WindowMenu } from './menu'
+import Github from './github'
+import Link from 'next/link'
+import useWebSocket, { ReadyState } from 'react-use-websocket'
+import { get_heatbeat_wss } from '@/app/lib/api'
+import {
+  Navbar,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from '@nextui-org/react'
+import Gallery from '../../home/gallery'
+import Settings from '../../home/settings'
 
 export default function Header() {
-    const { heartbeat, setHeartbeat } = useHeartbeatStore();
-    const { readyState } = useWebSocket(
-        get_heatbeat_wss(),
-        {
-            shouldReconnect: () => true,
-            heartbeat: {
-                message: 'ping',
-                returnMessage: 'pong',
-                timeout: 3000, // 3s
-                interval: HEART_BEAT_CHECK_1S,
-            },
-        }
-    );
+  const { heartbeat, setHeartbeat } = useHeartbeatStore()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    useEffect(() => {
-        if (readyState === ReadyState.OPEN) {
-            setHeartbeat(true);
-        } else {
-            setHeartbeat(false);
-        }
-    }, [readyState]);
+  const { readyState } = useWebSocket(get_heatbeat_wss(), {
+    shouldReconnect: () => true,
+    heartbeat: {
+      message: 'ping',
+      returnMessage: 'pong',
+      timeout: 3000, // 3s
+      interval: HEART_BEAT_CHECK_1S,
+    },
+  })
 
+  useEffect(() => {
+    if (readyState === ReadyState.OPEN) {
+      setHeartbeat(true)
+    } else {
+      setHeartbeat(false)
+    }
+  }, [readyState])
 
-    return (
-        <header className="text-gray-600 min-w-full h-min z-10">
-            {heartbeat ? null : <HeadAlert message={HEART_BEAT_ALERT} />}
-            <div className="flex flex-nowrap mx-auto p-1 md:p-5 flex-row items-center">
-                <Link href={"/"} className="flex title-font font-medium items-center text-gray-900 hover:text-gray-600">
-                    <img src="/icons/app_icon.svg" className="w-8 h-8 md:w-10 md:h-10 text-white p-2 rounded-full border-2 border-black" />
-                    <span className="ml-3 text-sm md:text-xl text-nowrap">{PROJ_NAME}</span>
-                </Link>
+  return (
+    <>
+      {heartbeat ? null : <HeadAlert message={HEART_BEAT_ALERT} />}
+      {/* 'backdrop-blur-xl backdrop-saturate-150' */}
 
-                <div className="mr-auto ml-2 md:ml-4 pl-2 md:pl-4 border-l border-gray-400">
-                    <WindowMenu />
-                </div>
-                {/* <div className="visible md:invisible">
-                    <PhoneMenu />
-                </div> */}
-                <div className="invisible md:visible">
-                    <Github />
-                </div>
+      <div className="flex text-gray-600 flex-nowrap w-full mx-auto p-1 md:p-5 flex-row justify-between items-center">
+        <Navbar
+          className={`flex justify-between ${
+            isMenuOpen
+              ? 'backdrop-blur-xl backdrop-saturate-150'
+              : 'md:!backdrop-filter-none bg-transparent !backdrop-blur-none'
+          } md:w-2/5 `}
+        >
+          <NavbarContent>
+            <Link
+              href={'/'}
+              className="flex title-font font-medium items-center text-gray-900 hover:text-gray-600 min-w-[290px]"
+            >
+              <img
+                src="/icons/app_icon.svg"
+                className="w-8 h-8 md:w-10 md:h-10 text-white p-2 rounded-full border-2 border-black"
+              />
+              <span className="ml-3 text-sm md:text-xl text-nowrap">
+                {PROJ_NAME}
+              </span>
+            </Link>
+
+            <div className="hidden md:block pl-4 border-l">
+              <WindowMenu />
             </div>
-        </header>
-    );
+          </NavbarContent>
+
+          <NavbarContent className="md:hidden" justify="end">
+            <NavbarMenuToggle
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen)
+              }}
+            />
+          </NavbarContent>
+
+          <NavbarMenu className="mt-10">
+            <NavbarMenuItem>
+              <Gallery />
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <Settings />
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <Link href="https://github.com/wan-h/awesome-digital-human-live2d">
+                GitHub
+              </Link>
+            </NavbarMenuItem>
+          </NavbarMenu>
+        </Navbar>
+
+        <div className="hidden md:block">
+          <Github />
+        </div>
+      </div>
+    </>
+  )
 }
